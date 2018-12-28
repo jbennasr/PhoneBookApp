f57838f59067851959816e56d8d64b442c0b09e6
Jaafer Ben Nasr <bennasr.jaafer@gmail.com>
2018-12-28 17:18:10 +0100

    AddContactClass

--- PhoneBookApp/PhoneBookConsole/Program.cs
+++ PhoneBookApp/PhoneBookConsole/Program.cs
@@ -10,6 +10,17 @@ namespace PhoneBookConsole
     {
         static void Main(string[] args)
         {
+
+            Contact contact = new Contact() {ContactID = 1,Name = "Ali Ben Ali",Mobil = "53565765",Email = "ali@gmail.com"};
+
+            List<Contact> contacts = new List<Contact>()
+            {
+                new Contact() { ContactID = 1, Name = "Ali Ben Ali", Mobil = "53565765", Email = "ali@gmail.com" },
+                new Contact() { ContactID = 2, Name = "maram tatoui", Mobil = "222222222", Email = "maram@gmail.com" },
+                new Contact() { ContactID = 1, Name = "Foued ben ", Mobil = "53565765", Email = "ali@gmail.com" },
+                new Contact() { ContactID = 1, Name = "Ali Ben Ali", Mobil = "53565765", Email = "ali@gmail.com" }
+
+            };
         }
     }
 }
