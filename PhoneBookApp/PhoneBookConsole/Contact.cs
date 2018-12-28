670f00821ec07445f21e07746da9f36564763e37
Jaafer Ben Nasr <jbennasr@vistaprint.com>
2018-12-28 16:57:58 +0100

    FirstCommit

---
+++ PhoneBookApp/PhoneBookConsole/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookConsole
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+        public string Name { get; set; }
+        public string Mobil { get; set; }
+        public string Email { get; set; }
+    }
+}
